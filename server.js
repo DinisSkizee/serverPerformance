f4f486ff176178c005611fd3de6015505618cc7a
Flávio Dinis de Oliveira Figueiredo Lopes da Silva <dinissilvauk@gmail.com>
2022-07-21 16:47:30 +0100

    first commit

---
+++ server.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cluster = require("cluster");
+const os = require("os");
+
+const app = express();
+
+const delay = (duration) => {
+  const startTime = Date.now();
+  while (Date.now() - startTime < duration) {}
+};
+
+app.get("/", (req, res) => {
+  // JSON.stringify({}) => "{}"
+  // JSON.parse("{}") => {}
+  // [5,1,2,3,4].sort()
+  res.send(`Performance example ${process.pid}`);
+});
+
+app.get("/timer", (req, res) => {
+  delay(9000);
+  res.send(`Ding ding ding! ${process.pid}`);
+});
+
+if (cluster.isMaster) {
+  console.log("Master has been started");
+  const NUM_WORKERS = os.cpus().length;
+  for (let i = 0; i < NUM_WORKERS; i++) {
+    cluster.fork();
+  }
+} else {
+  console.log("Worker Process started");
+  app.listen(3000);
+}
